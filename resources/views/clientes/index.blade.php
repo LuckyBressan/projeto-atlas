438824742e3a4261d5c2c5c97a01e1d4479028c0
LuckyBressan <lucas.a.dos.s.bressan@gmail.com>
2026-06-17 22:15:07 -0300

    feat: add empty-state quando não há clientes cadastrados;

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -12,55 +12,67 @@
                     Incluir
                 </a>
             </div>
+            @if ($clientes->isEmpty())
+                <!-- Empty state para quando não há nenhum cliente cadastrado -->
+                <x-empty-state title="Nada encontrado">
+                    <x-slot name="icon">
+                        <x-lucide-user-round-x/>
+                    </x-slot>
+                    <x-slot name="description">
+                        Você não cadastrou nenhum cliente ainda.<br>
+                        Comece criando algum.
+                    </x-slot>
+                </x-empty-state>
+            @else
+                <x-table>
+                    <x-slot name="header">
+                        <tr>
+                            <th>ID</th>
+                            <th>Nome</th>
+                            <th>CPF</th>
+                            <th>Data de Nascimento</th>
+                            <th></th>
+                        </tr>
+                    </x-slot>
+                    @foreach ($clientes as $cliente)
 
-            <x-table>
-                <x-slot name="header">
-                    <tr>
-                        <th>ID</th>
-                        <th>Nome</th>
-                        <th>CPF</th>
-                        <th>Data de Nascimento</th>
-                        <th></th>
-                    </tr>
-                </x-slot>
-                @foreach ($clientes as $cliente)
+                        <tr>
+                            <td>
+                                {{ $cliente->id }}
+                            </td>
+                            <td>
+                                {{ $cliente->nome }}
+                            </td>
+                            <td>
+                                {{ $cliente->cpf_formatted }}
+                            </td>
+                            <td>
+                                {{ $cliente->data_nascimento_formatted ?? '-' }}
+                            </td>
+                            <td>
+                                <a href="{{ route('processos.index', $cliente->id) }}" class="btn-outline"
+                                    data-tooltip="Ver processos">
+                                    <x-lucide-book-search/>
+                                </a>
+                                <a href="{{ route('clientes.edit', $cliente->id) }}" class="btn-outline btn-edit"
+                                    data-tooltip="Editar">
+                                    <x-lucide-edit/>
+                                </a>
+                                <!-- Sei que tá dando erro de sintaxe, ignore, pois está funcionando -->
+                                <button
+                                    type="button"
+                                    onclick="clientes.confirmarExclusao('{{ route('clientes.delete', $cliente->id) }}', 'alert-delete')"
+                                    class="btn-outline btn-delete aspect-square"
+                                    data-tooltip="Excluir"
+                                >
+                                    <x-lucide-trash-2/>
+                                </button>
+                            </td>
+                        </tr>
 
-                    <tr>
-                        <td>
-                            {{ $cliente->id }}
-                        </td>
-                        <td>
-                            {{ $cliente->nome }}
-                        </td>
-                        <td>
-                            {{ $cliente->cpf_formatted }}
-                        </td>
-                        <td>
-                            {{ $cliente->data_nascimento_formatted ?? '-' }}
-                        </td>
-                        <td>
-                            <a href="{{ route('processos.index', $cliente->id) }}" class="btn-outline"
-                                data-tooltip="Ver processos">
-                                <x-lucide-book-search/>
-                            </a>
-                            <a href="{{ route('clientes.edit', $cliente->id) }}" class="btn-outline btn-edit"
-                                data-tooltip="Editar">
-                                <x-lucide-edit/>
-                            </a>
-                            <!-- Sei que tá dando erro de sintaxe, ignore, pois está funcionando -->
-                            <button
-                                type="button"
-                                onclick="clientes.confirmarExclusao('{{ route('clientes.delete', $cliente->id) }}', 'alert-delete')"
-                                class="btn-outline btn-delete aspect-square"
-                                data-tooltip="Excluir"
-                            >
-                                <x-lucide-trash-2/>
-                            </button>
-                        </td>
-                    </tr>
-
-                @endforeach
-            </x-table>
+                    @endforeach
+                </x-table>
+            @endif
         </div>
 
 
