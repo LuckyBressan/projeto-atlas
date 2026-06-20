81390e2cc9141e601172b1298e5e49d4cfebeb9e
LuckyBressan <lucas.a.dos.s.bressan@gmail.com>
2026-06-20 15:19:34 -0300

    feat: migrado front-end de processos para o novo padrão;

--- resources/views/processos/create.blade.php
+++ resources/views/processos/create.blade.php
@@ -2,56 +2,69 @@
 
 @section('title', $title)
 
-
-
 @section('content')
 
-    <h1>{{ $title }}</h1>
+    <div class="grid gap-2">
+        <a href="{{ route('processos.index', $cliente->id) }}" class="btn-outline w-max">
+            <x-lucide-arrow-big-left-dash />
+            Voltar
+        </a>
+        <x-card title="{{ $title }}" description="Informe os dados para inclusão do processo">
+            <div class="grid gap-6">
+                @if (!$errors->isEmpty())
+                    <fieldset class="grid gap-2">
+                        @foreach ($errors->all() as $err)
+                            <x-alert title="Ops! Algo deu errado" type="destructive">
+                                <x-slot name="icon">
+                                    <x-lucide-circle-alert />
+                                </x-slot>
+                                {{ $err }}
+                            </x-alert>
+                        @endforeach
+                    </fieldset>
 
-    <a href="{{ route('processos.index', $cliente->id) }}">Home</a>
+                @endif
 
-    <br>
-    <br>
+                <x-form action="{{ route('processos.store') }}" method="post">
+                    @csrf
+                    <!-- Id do cliente para que possamos cadastrar novo processo -->
+                    <input name="cliente_id" type="hidden" value="{{ $cliente->id }}" class="hidden">
 
-    @if ($errors->any)
-        @foreach ($errors->all() as $err)
-            <div style="color: red;">
-                {{ $err }}
+                    <x-select-search
+                        id="livro"
+                        title="Livro"
+                        classButton="w-full"
+                        :options="$livros"
+                    />
+
+                    <x-textarea id="observacao" title="Observação" placeholder="Ex: livro devolvido/retirado com defeito" />
+
+                    <fieldset class="grid grid-cols-2 gap-3">
+                        <x-input type="date" id="data_retirada" title="Data de Retirada" value="{{ Date::now()->toDateString() }}" />
+
+                        <x-input type="date" id="data_prevista" title="Data Prevista"
+                            value="{{ Date::now()->addDays(30)->toDateString() }}" />
+                    </fieldset>
+
+
+                    <fieldset class="grid gap-3">
+                        <label>
+                            Status
+                        </label>
+                        <div class="flex gap-3">
+                            @foreach ([App\Enums\ProcessoStatus::ABERTO, App\Enums\ProcessoStatus::FECHADO] as $status)
+                                <x-radio
+                                    name="status"
+                                    id="status_{{ $status->value }}"
+                                    value="{{ $status->value }}"
+                                    checked="{{ $status->value == App\Enums\ProcessoStatus::ABERTO->value }}"
+                                    label="{{ $status->name }}"
+                                />
+                            @endforeach
+                        </div>
+                    </fieldset>
+                </x-form>
             </div>
-        @endforeach
-    @endif
-
-    <form action="{{ route('processos.store') }}" method="post">
-        @csrf
-        <input type="hidden" name="cliente_id" value="{{ $cliente->id }}">
-        <div>
-            <label for="livro_id">Livro</label>
-            <select name="livro_id" id="livro_id">
-                <option value="">Selecione um livro</option>
-                @foreach ($livros as $livro)
-                    <option value="{{ $livro->id }}" @selected(old('livro_id') == $livro->id)>
-                        {{ $livro->titulo }}
-                    </option>
-                @endforeach
-            </select>
-        </div>
-        <div>
-            <label for="observacao">Observação</label>
-            <textarea name="observacao" id="observacao"></textarea>
-        </div>
-        <div>
-            <label for="data_retirada">Data de Retirada</label>
-            <input type="date" name="data_retirada" id="data_retirada" value="{{ Date::now()->toDateString() }}">
-        </div>
-        <div>
-            <label for="data_prevista">Data Prevista</label>
-            <input type="date" name="data_prevista" id="data_prevista" value="{{ Date::now()->addDays(30)->toDateString() }}">
-        </div>
-        <div>
-            <label for="status">Status</label>
-            <input type="radio" name="status" value="{{ App\Enums\ProcessoStatus::ABERTO }}" checked>Aberto
-            <input type="radio" name="status" value="{{ App\Enums\ProcessoStatus::FECHADO }}" >Fechado
-        </div>
-        <input type="submit" value="Enviar">
-    </form>
+        </x-card>
+    </div>
 @endsection
