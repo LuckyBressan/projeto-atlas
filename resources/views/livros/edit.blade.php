0450ffb28b283e3417189c2053546df50dc97faf
JuFitzlaff <julia.fitzlaff@unidavi.edu.br>
2026-06-21 11:05:49 -0300

    feat: padroniza telas de livros com novos componentes

--- resources/views/livros/edit.blade.php
+++ resources/views/livros/edit.blade.php
@@ -3,48 +3,81 @@
 @section('title', $title)
 
 @section('content')
+    <div class="grid gap-2">
+        <a href="{{ route('livros.index') }}" class="btn-outline w-max">
+            <x-lucide-arrow-big-left-dash/>
+            Voltar
+        </a>
 
-    <h1>{{ $title }}</h1>
+        <x-card title="{{ $title }}" description="Altere os dados do livro cadastrado">
+            <div class="grid gap-6">
+                @if (!$errors->isEmpty())
+                    <fieldset class="grid gap-3">
+                        @foreach ($errors->all() as $err)
+                            <x-alert title="Ops! Algo deu errado" type="destructive">
+                                <x-slot name="icon">
+                                    <x-lucide-circle-alert/>
+                                </x-slot>
+                                {{ $err }}
+                            </x-alert>
+                        @endforeach
+                    </fieldset>
+                @endif
 
-    <a href="{{ route('livros.index') }}">Voltar</a>
+                <x-form action="{{ route('livros.update', $livro->id) }}" method="post">
+                    @csrf
+                    @method('PATCH')
 
-    <br>
-    <br>
+                    <x-input
+                        id="titulo"
+                        title="Título"
+                        placeholder="Ex: Dom Casmurro"
+                        value="{{ old('titulo', $livro->titulo) }}"
+                        required
+                    />
 
-    @if ($errors->any())
-        @foreach ($errors->all() as $err)
-            <div style="color: red;">
-                {{ $err }}
+                    <x-input
+                        id="autor"
+                        title="Autor"
+                        placeholder="Ex: Machado de Assis"
+                        value="{{ old('autor', $livro->autor) }}"
+                        required
+                    />
+
+                    <x-textarea
+                        id="descricao"
+                        title="Descrição"
+                        placeholder="Ex: Romance clássico da literatura brasileira"
+                    >{{ old('descricao', $livro->descricao) }}</x-textarea>
+
+                    <fieldset class="grid gap-2">
+                        <label for="categoria_id">Categoria</label>
+                        <select name="categoria_id" id="categoria_id" class="select" required>
+                            @foreach ($categorias as $categoria)
+                                <option value="{{ $categoria->id }}" @selected(old('categoria_id', $livro->categoria_id) == $categoria->id)>
+                                    {{ $categoria->titulo }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </fieldset>
+
+                    <fieldset class="grid gap-3">
+                        <label>Status</label>
+                        <div class="flex gap-3">
+                            @foreach ([App\Enums\LivroStatus::DISPONIVEL, App\Enums\LivroStatus::EMPRESTADO] as $status)
+                                <x-radio
+                                    name="status"
+                                    id="status_{{ $status->value }}"
+                                    value="{{ $status->value }}"
+                                    checked="{{ old('status', $livro->status) === $status->value }}"
+                                    label="{{ ucfirst(strtolower($status->name)) }}"
+                                />
+                            @endforeach
+                        </div>
+                    </fieldset>
+                </x-form>
             </div>
-        @endforeach
-    @endif
-
-    <form action="{{ route('livros.update', $livro->id) }}" method="post">
-        @csrf
-        @method('PATCH')
-        <div>
-            <label for="titulo">Título</label>
-            <input type="text" name="titulo" id="titulo" value="{{ old('titulo', $livro->titulo) }}">
-        </div>
-        <div>
-            <label for="autor">Autor</label>
-            <input type="text" name="autor" id="autor" value="{{ old('autor', $livro->autor) }}">
-        </div>
-        <div>
-            <label for="descricao">Descrição</label>
-            <textarea name="descricao" id="descricao">{{ old('descricao', $livro->descricao) }}</textarea>
-        </div>
-        <div>
-            <label for="categoria_id">Categoria</label>
-            <select name="categoria_id" id="categoria_id">
-                @foreach ($categorias as $categoria)
-                    <option value="{{ $categoria->id }}" @selected(old('categoria_id', $livro->categoria_id) == $categoria->id)>
-                        {{ $categoria->titulo }}
-                    </option>
-                @endforeach
-            </select>
-        </div>
-        <input type="submit" value="Salvar">
-    </form>
+        </x-card>
+    </div>
 
 @endsection
